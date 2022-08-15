c2488f61e3ff566f39f26467bdfa528cead48ec5
Ryan paik <ryanpaik@protonmail.com>
2022-08-15 07:56:34 -0700

    mbair sync

---
+++ Playgrounds/00_Snippets_PG.playground/Pages/ARRAYS.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Empty Arrary of type Int
+var SomeInts: [Int] = []
+print("someInts   of type [Int] with \(someInts.count) items.")
+
+
+// Array
+
+//: [Next](@next)
--- Playgrounds/00_Snippets_PG.playground/Pages/DataTypes.xcplaygroundpage/Contents.swift
+++ Playgrounds/00_Snippets_PG.playground/Pages/DataTypes.xcplaygroundpage/Contents.swift
@@ -2,12 +2,162 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+//: ## BASIC DATATYPES
+//: Number Types: Int, DOuble, Float
+//: Boolean: Bool
+//: Textual: String Character
+//: Collection Types: Array, Set, Dictionaries, Tuples
 
-//: Strings
+//: ## CONSTANTS VARIABLES
+
+let sampleConstat = 10
+var sampleVar = 0
+var x = 0.0, y = 0.0, z = 0.0
+
+// type annotations
+var welcomeMessage: String
+var red, green, blue: Double
+
+// Variables and Assignment
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+
+// Constants cannot be mutated
+//let languageName = "Swift"
+//languageName = "Swift++"
+
+print(friendlyWelcome)
+
+
+// NUMERIC LITERALS
+let decimalInteger = 17
+let binaryInteger = 0b10001       // 17 in binary notation
+let octalInteger = 0o21           // 17 in octal notation
+let hexadecimalInteger = 0x11     // 17 in hexadecimal notation
+let decimalDouble = 12.1875
+let exponentDouble = 1.21875e1
+let hexadecimalDouble = 0xC.3p0
+let paddedDouble = 000123.456
+let oneMillion = 1_000_000
+let justOverOneMillion = 1_000_000.000_000_1
+
+
+
+// NUMERIC TYPE CONVERSION
+let cannotBeNegative: UInt8 = -1
+// UInt8 can't store negative numbers, and so this will report an error
+let tooBig: Int8 = Int8.max + 1
+// Int8 can't store a number larger than its maximum value,
+// and so this will also report an error
+let twoThousand: UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
+
+// numeric adn float conversion
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi = Double(three) + pointOneFourOneFiveNine
+// pi equals 3.14159, and is inferred to be of type Double
+
+let integerPi = Int(pi)
+// integerPi equals 3, and is inferred to be of type Int
+
+/* ----------------- */
+// TYPE ALIASES
+typealias AudioSample = UInt16
+var maxAmplitudeFound = AudioSample.min
+// maxAmplitudeFound is now 0
+
+/* ----------------- */
+// BOOLEANS
+let orangesAreOrange = true
+let turnipsAreDelicious = false
+
+if turnipsAreDelicious {
+    print("Mmm, tasty turnips!")
+} else {
+    print("Eww, turnips are horrible.")
+}
+// Prints "Eww, turnips are horrible."
+
+
+/* ----------------- */
+// TUPLES
+let http404Error = (404, "Not Found")
+// http404Error is of type (Int, String), and equals (404, "Not Found")
+
+let (statusCode, statusMessage) = http404Error
+print("The status code is \(statusCode)")
+// Prints "The status code is 404"
+print("The status message is \(statusMessage)")
+// Prints "The status message is Not Found"
+
+let (justTheStatusCode, _) = http404Error
+print("The status code is \(justTheStatusCode)")
+// Prints "The status code is 404
+
+print("The status code is \(http404Error.0)")
+// Prints "The status code is 404"
+print("The status message is \(http404Error.1)")
+// Prints "The status message is Not Found"
+
+let http200Status = (statusCode: 200, description: "OK")
+
+print("The status code is \(http200Status.statusCode)")
+// Prints "The status code is 200"
+print("The status message is \(http200Status.description)")
+// Prints "The status message is OK"
+
+
+/* ----------------- */
+//: ## OPTIONALS
+let possibleNumber = "123"
+let convertedNumber = Int(possibleNumber)
+// convertedNumber is inferred to be of type "Int?", or "optional Int"
+
+//nil
+var serverResponseCode: Int? = 404
+// serverResponseCode contains an actual Int value of 404
+serverResponseCode = nil
+// serverResponseCode now contains no value
+
+// nil default value
+var surveyAnswer: String?
+// surveyAnswer is automatically set to nil
+
+//checking for nil
+if convertedNumber != nil {
+    print("convertedNumber contains some integer value.")
+}
+// Prints "convertedNumber contains some integer value."
+
+
+// OPTIONAL BINDING
+//You use optional binding to find out whether an optional contains a value, and if so, to make that value available as a temporary constant or variable. Optional binding can be used with if and while statements to check for a value inside an optional, and to extract that value into a constant or variable, as part of a single action
+
+//if let constantName = someOptional {
+//    statements
+//}
+
+if let actualNumber = Int(possibleNumber) {
+    print("The string \"\(possibleNumber)\" has an integer value of \(actualNumber)")
+} else {
+    print("The string \"\(possibleNumber)\" couldn't be converted to an integer")
+}
+// Prints "The string "123" has an integer value of 123"
+
+
+
+
+
+
+/* ----------------- */
+//: ## STRINGS
 /*:
  Swift’s String type is a value type. If you create a new String value, that String value is copied when it’s passed to a function or method, or when it’s assigned to a constant or variable. In each case, a new copy of the existing String value is created, and the new copy is passed or assigned, not the original version. Value types are described in Structures and Enumerations Are Value Types.
  */
+
+
 var emptyString = ""               // empty string literal
 var anotherEmptyString = String()
 
